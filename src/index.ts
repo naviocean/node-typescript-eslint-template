b51b14b1198063aaab9058594e2f773fcf99d24d
naviocean <navi.ocean@outlook.com>
2020-11-10 15:06:40 +0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import chalk = require("chalk");
 
 const { log } = console;
 
+const wait = (ms: number): Promise<void> => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  log(chalk.green("Hello world!"));
-  log("🚀");
+    log(chalk.green("Hello world!"));
+    await wait(1000);
+    log("🚀");
 })();
